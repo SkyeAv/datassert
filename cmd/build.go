81ea54af6beffd5faed5a5da85cfc097ff8546fb
SkyeAv <skye.lane.goetz@gmail.com>
2026-03-16 14:44:57 -0700

    Wrote Encoder Decoder BoilerPlate

--- cmd/build.go
+++ cmd/build.go
@@ -2,9 +2,11 @@ package cmd
 
 import (
 	"log"
+	"os"
 	"path/filepath"
 	"sync"
 
+	"github.com/klauspost/compress/zstd"
 	"github.com/spf13/cobra"
 )
 
@@ -32,15 +34,59 @@ type ClassLookup struct {
 	data map[string][]string
 }
 
+func NewClassLookup() *ClassLookup {
+	return &ClassLookup{data: make(map[string][]string)}
+}
+
 func (cl *ClassLookup) Set(key string, value []string) {
 	cl.mu.Lock()
 	cl.data[key] = value
 	cl.mu.Unlock()
 }
 
+type ClassRecord struct {
+	EquivalentIdentifiers []string `json:"equivalent_identifiers"`
+}
+
+func parseClassFile(fileName string, cl *ClassLookup, wg *sync.WaitGroup) {
+	f, err := os.Open(fileName)
+	if err != nil {
+		throwError(3, err)
+	}
+
+	zr, err := zstd.NewReader(f)
+	if err != nil {
+		throwError(4, err)
+	}
+
+	decoder := sonic.ConfigDefault.NewDecoder(zr)
+
+	f.Close()
+	zr.Close()
+	wg.Done()
+}
+
+func buildClassLookup(fileNames []string) *ClassLookup {
+	cl := NewClassLookup()
+	wg := sync.WaitGroup{}
+
+	for _, fileName := range fileNames {
+		wg.Add(1)
+		go parseClassFile(fileName, cl, &wg)
+	}
+
+	wg.Wait()
+	return cl
+}
+
 func build(cmd *cobra.Command, args []string) {
-	synonymDir := args[0]
-	classDir := args[1]
+	classDir := args[0]
+	synonymDir := args[1]
+
+	classFileNames := globFileNames(classDir)
+	cl := buildClassLookup(classFileNames)
+
+	synonymFileNames := globFileNames(synonymDir)
 }
 
 var buildCmd = &cobra.Command{
